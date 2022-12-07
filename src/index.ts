9910cb9bcccb82b67f8b567e89d752f8b46517a5
angelsflyinhell <azami@namespace.media>
2022-12-07 16:59:24 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,26 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     console.log(req.clientIp);
     res.setHeader('Content-Type', 'text/html')
-    res.send('<h2>Test String</h2>');
+    res.send(`
+        <html>
+            <head>
+                <meta http-equiv="content-Type" content="text/html; utf-8" />
+                <meta http-equiv="Pragma" content="no-cache" />
+                <meta name="robots" content="NOINDEX,NOFOLLOW" />
+                <meta http-equiv="content-Language" content="en" />
+                <meta name="description" content="" />
+                <meta name="keywords" content="" />
+                <meta name="author" content="" />
+                <meta name="publisher" content="" />
+                <meta name="copyright" content="" />
+                <meta http-equiv="Reply-to" content="" />
+                <meta name="expires" content="" />
+                <meta name="revisit-after" content="2 days" />
+                <title>index.html</title>
+            </head>
+            <body>
+                <h1>index.html</h1>
+            </body>
+        </html>
+    `);
 });
